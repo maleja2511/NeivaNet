7b74d18cc595f568bb71e2857b006985ea1e7cc9
LauraFalla1 <fallalaura350gmail.com>
2023-09-06 21:23:32 -0500

    url_index

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ from .views import update_profile
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
     path('update_profile/', update_profile, name='update_profile'),
     path('logout/', LogoutView.as_view(), name='logout'),
