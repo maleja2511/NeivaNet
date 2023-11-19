e9218837c33d24251f059455fb8abf6bc8f80918
David Valencia <davidvalencia0526@gmail.com>
2023-11-18 22:43:00 -0500

    implementada validacion de email

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import UserProfile
+from django.core.exceptions import ValidationError
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -9,7 +10,17 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
-        
+
+    def clean_email(self):
+        # Obtén el correo electrónico proporcionado en el formulario
+        email = self.cleaned_data['email']
+
+        # Verifica si ya existe un usuario con ese correo electrónico
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Ya existe un usuario con ese correo electrónico.")
+
+        return email
+
 
 class UserProfileForm(forms.ModelForm):
     date_of_birth = forms.DateField(
