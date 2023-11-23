135fdde6c2290e15a1fcefb06c4bf67b2a989fa1
David Valencia <davidvalencia0526@gmail.com>
2023-11-22 20:46:43 -0500

    implementado punto rojo notificaciones no vistas

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_notifications, notification_list
+from .views import count_unread_notifications, get_notifications, notification_list
 
 urlpatterns = [
     path('notifications/', get_notifications, name='get-notifications'),
     path('my-notifications/', notification_list, name='notification-list'),
+    path('unread-notifications-count/', count_unread_notifications, name='unread-notifications-count'),
 ]
